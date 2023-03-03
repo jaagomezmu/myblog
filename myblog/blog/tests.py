8bebca560802b1779a66bb1fc31c8053039d1017
Javier Alejandro Gómez Muñoz <jaagomezmu@unal.edu.co>
2023-03-03 14:42:32 -0500

    Update unit test names

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -15,20 +15,20 @@ class TestPostModel:
     author = ''
     title = 'x'*100
     
-    def test_creation_blog_post_1(self, user_1):
+    def test_creation_blog_post_in_postgres(self, user_1):
         self.post = BlogPost.objects.create(title = 'test1',
                                     body = 'This is the body of the first test',
                                     author = user_1)        
         assert BlogPost.objects.count() == self.length
     
-    def test_creation_blog_post_2(self, user_1):
+    def test_foreign_key_in_blog_post_model(self, user_1):
         with pytest.raises(ValueError) as exc_value:
             post = BlogPost.objects.create(title = 'test2',
                                            body = 'This is the body of the second test',
                                         #    author = user_1,
                                            author = self.author,
                                             )
-    def test_creation_blog_post_3(self, user_1):
+    def test_display_name_of_blog_post_model(self, user_1):
         post = BlogPost.objects.create(title = self.title,
                                        body = 'This is the body of the third test',
                                        author = user_1,
