81105fd5f715556a23ed37fd754cb1ce8c8bbd15
Javier Alejandro <jaagomezmu@unal.edu.co>
2023-03-14 17:47:33 -0500

    Refactor serializer with a meta class

--- myblog/blog/api/serializers.py
+++ myblog/blog/api/serializers.py
@@ -3,10 +3,6 @@ from rest_framework import serializers
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
-    # Declare the new names to the serializer
-    # This is necessary because my model does  not have the same names as the exercise requests.
-    text = serializers.CharField(source = 'title')
-    user = serializers.IntegerField(source = 'author.id')
     class Meta:
         model = BlogPost
-        fields = ('body', 'text', 'user')
+        fields = ('body', 'title', 'author')
