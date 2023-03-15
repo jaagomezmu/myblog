e742a91b0a4edf169e8bfb2415e74cd2abf33863
Javier Alejandro <jaagomezmu@unal.edu.co>
2023-03-15 09:05:04 -0500

    Refactor with a class the api get post

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns=[
   path('register', views.register_request, name='register'),
   path("login", views.login_request, name="login"),
   path("logout", views.logout_request, name= "logout"),
-  path('api/post', views.blogpost_api_view, name="api/post")
+  path("api/post", views.PostsViewSet.as_view(), name="api/post")
 ]
