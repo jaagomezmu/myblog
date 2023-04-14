8dbd29d1ded483eca990ae4df23290b537a13226
Javier Alejandro <jaagomezmu@unal.edu.co>
2023-04-13 19:00:16 -0500

    Refactor url file with routers and add one action

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -14,24 +14,3 @@ urlpatterns = [
     path('api/post/my-tags/', views.PostViewSet.as_view({'get': 'my_tags'}), name='my-tags'),
     path('', include(router.urls)),
 ]
-
-
-# urlpatterns=[
-#   path('',views.index, name='index'),
-#   path('register', views.register_request, name='register'),
-#   path("login", views.login_request, name="login"),
-#   path("logout", views.logout_request, name= "logout"),
-#   path("api/post", views.PostViewSet.as_view({'get': 'list',
-#                                               'post':'create'}),
-#        name="api/post"),
-#   path('api/post/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve',
-#                                                         'patch': 'partial_update',
-#                                                         'delete': 'destroy'}),
-#        name='post_detail'),
-#   path("api/comment", views.CommentViewSet.as_view({'get': 'list','post':'create'}),
-#        name="comment-list"),
-#   path('api/comment/<int:pk>/', views.CommentViewSet.as_view({'get': 'retrieve',
-#                                                               'patch': 'partial_update',
-#                                                               'delete': 'destroy'}),
-#        name='comment-detail'),
-# ]
