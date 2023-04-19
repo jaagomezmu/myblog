f0ea8f8d6aa8efecde2d4847a366d9a5dbb21347
Javier Alejandro <jaagomezmu@unal.edu.co>
2023-04-19 11:56:49 -0500

    Create actions for my-tags

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,22 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
-urlpatterns=[
-  path('',views.index, name='index'),
-  path('register', views.register_request, name='register'),
-  path("login", views.login_request, name="login"),
-  path("logout", views.logout_request, name= "logout"),
-  path("api/post", views.PostViewSet.as_view({'get': 'list',
-                                              'post':'create'}),
-       name="api/post"),
-  path('api/post/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve',
-                                                        'patch': 'partial_update',
-                                                        'delete': 'destroy'}),
-       name='post_detail'),
-  path("api/comment", views.CommentViewSet.as_view({'get': 'list','post':'create'}),
-       name="comment-list"),
-  path('api/comment/<int:pk>/', views.CommentViewSet.as_view({'get': 'retrieve',
-                                                              'patch': 'partial_update',
-                                                              'delete': 'destroy'}),
-       name='comment-detail'),
+router = routers.DefaultRouter()
+router.register(r'api/post', views.PostViewSet, basename="post")
+router.register(r'api/comment', views.CommentViewSet)
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('', include(router.urls)),
 ]
